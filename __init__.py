f7e0fa4a9499e947a2f2c171b8a867b99d75da3f
y24m007Y <y24m007@mail.ryukoku.ac.jp>
2025-09-19 14:54:13 +0900

    Second commit from issue1

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from flask import Flask
 app = Flask(__name__)
-import webapp.views
+import SearchApp.views
--- views.py
+++ views.py
@@ -1,22 +1,33 @@
-from flask import render_template
-from webapp import app
-from Searcher import BM25, VecSearch, searcher
-
-model = searcher.searcher()
-#bm25 = BM25.bm25()
-#vec_model = VecSearch.vec_search()
+from flask import render_template, url_for, request
+from SearchApp import app
+from QiitaSearch import BM25, VecSearch, RankFusion, searcher
 
 @app.route('/')
 def create_app():
-    return "Hello, World!"
+    return render_template('index.html')
 
-@app.route('/result_page')
-def result_page():
-    query = "python"
-    result_data = model.search(query, top_n=20)
-    return render_template('result_page.html', query=query, results=result_data)
+@app.route('/search_page', methods=['GET', 'POST'])
+def search_page():
+    if request.method == "POST":
+        query = request.form['query']
+        argorithm = request.form['argoritm']
+        if argorithm=="0":
+            engin = BM25.bm25()
+        elif argorithm=="1":
+            engin = VecSearch.vec_search()
+        elif argorithm=="2":
+            engin = RankFusion.RRF()
+        else:
+            print("適切なフォームが送信されていません")
+            return
+        results = engin.search(query)
+        return render_template('/result_page.html', query=query, results=results)
+    else:
+        return render_template('/search_page.html')
 
+"""
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello_world(name=None):
     return render_template('hello.html', name=name)
+"""
