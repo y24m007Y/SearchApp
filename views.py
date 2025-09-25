06eff452f6278fa2fea8b7ab0942425cf23b1130
y24m007Y <y24m007@mail.ryukoku.ac.jp>
2025-09-25 16:13:43 +0900

    first commit from tag_search

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, request
 from SearchApp import app
-from QiitaSearch import BM25, VecSearch, RankFusion, searcher
+from QiitaSearch import BM25, VecSearch, RankFusion, search_dict
 
 @app.route('/')
 def create_app():
@@ -11,6 +11,7 @@ def search_page():
     if request.method == "POST":
         query = request.form['query']
         argorithm = request.form['argoritm']
+        tags = request.form.getlist('tag')
         if argorithm=="0":
             engin = BM25.bm25()
         elif argorithm=="1":
@@ -20,10 +21,16 @@ def search_page():
         else:
             print("適切なフォームが送信されていません")
             return
-        results = engin.search(query)
-        return render_template('/result_page.html', query=query, results=results)
+        results = engin.search(query, tags)
+        print(tags)
+        return render_template('/result_page.html', query=query, results=results, tags=tags)
     else:
-        return render_template('/search_page.html')
+        tagdb = search_dict.tagDB()
+        tagdb.connect_database()
+        top10_tags = tagdb.getTaglist(top_n=10, sorted=True)
+        tagdb.close_database()
+        del tagdb
+        return render_template('/search_page.html', tags=top10_tags)
 
 """
 @app.route('/hello/')
