0603589bcfcdccd3d7be6e6cd2fb89ad8c10253c
y24m007Y <y24m007@mail.ryukoku.ac.jp>
2025-10-30 16:41:54 +0900

    delete SearchApp from app

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 from flask import render_template, url_for, request, redirect, session, g, jsonify
-from SearchApp import app
-from .QiitaSearch import BM25, VecSearch, RankFusion, search_dict, TagComb
+from QiitaSearch import BM25, VecSearch, RankFusion, search_dict, TagComb
 import numpy as np
 import colorsys
 import asyncio
