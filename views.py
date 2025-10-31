e9973593f517b21769dc630a259b1657fee23189
y24m007Y <y24m007@mail.ryukoku.ac.jp>
2025-10-31 16:30:16 +0900

    DATABASE URL print

--- views.py
+++ views.py
@@ -20,6 +20,7 @@ text_generator = AsyncOpenAI(api_key=api_key)
 explainer = OpenAI(api_key=api_key)
 
 print("views.py")
+print(os.getenv('DATABASE_URL')
 
 from flask import Blueprint
 bp = Blueprint("main_bp", __name__)
